74bd04f5d74a5203040f0ac62f8916d8ef03c0a3
nixon <nixondutta402@gmail.com>
2023-12-05 13:15:07 +0900

    finish reproudcing the results -170

--- run_market2duke.sh
+++ run_market2duke.sh
@@ -1,11 +1,11 @@
 ## step 1 Source-domain pre-training
-# Duke to Market
+# Market to Duke
 python source_pretrain.py -ds market -dt duke --data-dir /mnt/AIProjects/trinh/DATA/reid --logs-dir logs/market2duke/source_pretraining -b 128
 
 ## step 2 Target-domain fine-tuning
-# Duke to Market
+# Market to Duke
 python target_train.py -dt duke --data-dir /mnt/AIProjects/trinh/DATA/reid --logs-dir logs/market2duke/target_fine_tuning --initial-weights logs/market2duke/source_pretraining -b 128
 
 ## step 3 Evaluate in the target domain
-# Duke to Market
+# Market to Duke
 python model_test.py -dt duke --data-dir /mnt/AIProjects/trinh/DATA/reid --resume logs/market2duke/target_fine_tuning/model_best.pth.tar 
