e7b4d6170005cf0934e702ba1ed9c8705380b466
nixon <nixondutta402@gmail.com>
2024-07-16 17:21:23 +0900

    Fix paths

--- reid/datasets/cuhk03np.py
+++ reid/datasets/cuhk03np.py
@@ -20,7 +20,7 @@ class CUHK03NP(BaseImageDataset):
     # identities: ??? (+1 for background)
     # images: ??? (train) + 1400 (query) + ??? (gallery)
     """
-    dataset_dir = '/old/home/ccvn/Workspace/trinh/data/reid/cuhk03np'
+    dataset_dir = '/mnt/AIProjects/trinh/DATA/reid/cuhk03np'
 
     def __init__(self, root, verbose=True, **kwargs):
         super(CUHK03NP, self).__init__()
