5c53a11be97aa56c6d92af07cadb0bd98bfa20bc
Trinh PRO HPC3 <nguyenquoctrinhctt3@gmail.com>
2024-10-24 17:27:15 +0900

    update veri2vric

--- run_VeRi2VRIC.sh
+++ run_VeRi2VRIC.sh
@@ -16,21 +16,21 @@
 #                          --logs-dir logs/VeRi2VRIC/resnet101/source_256 -b 128 --arch resnet101_source \
 #                          --height 256 --width 256
 
-# ## step 2 Target-domain fine-tuning                        
-# CUDA_VISIBLE_DEVICES=1,2 python target_train.py -dt vric --data-dir /old/home/ccvn/Workspace/trinh/data/reid \
-#                         --logs-dir logs/VeRi2VRIC/resnet101/fine_256 \
-#                         --initial-weights logs/VeRi2VRIC/resnet101/source_256 -b 128 \
-#                         --num-clusters 5000 --arch resnet101 \
-#                         --height 256 --width 256
-
-# CUDA_VISIBLE_DEVICES=1,2 python target_train.py -dt vric --data-dir /old/home/ccvn/Workspace/trinh/data/reid \
-#                         --logs-dir logs/VeRi2VRIC/resnet101/fine_256 \
-#                         --initial-weights logs/VeRi2VRIC/resnet101/source_256 -b 128 \
-#                         --num-clusters 5000 --arch resnet101 \
-#                         --height 256 --width 256
-
-CUDA_VISIBLE_DEVICES=1,2 python model_test.py -dt vric --data-dir /old/home/ccvn/Workspace/trinh/data/reid \
-                        --resume logs/VeRi2VRIC/resnet101/fine_256/model_best.pth.tar -b 128 \
-                        --num-classes 5000 --arch resnet101 \
+## step 2 Target-domain fine-tuning                        
+CUDA_VISIBLE_DEVICES=1,2 python target_train.py -dt vric --data-dir /old/home/ccvn/Workspace/trinh/data/reid \
+                        --logs-dir logs/VeRi2VRIC/resnet101/fine_256s_5000c \
+                        --initial-weights logs/VeRi2VRIC/resnet101/source_256 -b 128 \
+                        --num-clusters 5000 --arch resnet101 \
                         --height 256 --width 256
+
+# CUDA_VISIBLE_DEVICES=1,2 python target_train.py -dt vric --data-dir /old/home/ccvn/Workspace/trinh/data/reid \
+#                         --logs-dir logs/VeRi2VRIC/resnet101/fine_256 \
+#                         --initial-weights logs/VeRi2VRIC/resnet101/source_256 -b 128 \
+#                         --num-clusters 5000 --arch resnet101 \
+#                         --height 256 --width 256
+
+# CUDA_VISIBLE_DEVICES=1,2 python model_test.py -dt vric --data-dir /old/home/ccvn/Workspace/trinh/data/reid \
+#                         --resume logs/VeRi2VRIC/resnet101/fine_256/model_best.pth.tar -b 128 \
+#                         --num-classes 5000 --arch resnet101 \
+#                         --height 256 --width 256
                     
