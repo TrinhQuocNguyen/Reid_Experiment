ba3b4c1b1d5ded19736f020e2f9e48e3fdeb274c
Trinh <nguyenquoctrinhctt3@gmail.com>
2025-03-04 18:40:35 +0900

    update save features and evaluators

--- model_save_feature_maps.py
+++ model_save_feature_maps.py
@@ -105,6 +105,11 @@ def main_worker(args):
     # Evaluator
     print("Test on the target domain of {}:".format(args.dataset_target))
     evaluator_ = Evaluator(encoder)
+    
+    if not os.path.exists("./feature_maps/ori"):
+        os.makedirs("./feature_maps/ori")
+    if not os.path.exists("./feature_maps/flip"):
+        os.makedirs("./feature_maps/flip")
 
     feature_map_ori, feature_map_flip, _ = evaluator_.save_feature_maps(test_loader_target, source=False, save_path="./feature_heatmaps")
     save_feature_maps(feature_map_ori, save_path="./feature_maps/ori")
